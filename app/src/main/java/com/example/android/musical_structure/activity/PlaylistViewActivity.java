91b353397d3ec88ed17f201d432499aa254485ad
Sajid Zaman <sajid-619@users.noreply.github.com>
2016-06-30 22:05:29 +0600

    Create PlaylistViewActivity.java

---
+++ app/src/main/java/com/example/android/musical_structure/activity/PlaylistViewActivity.java
@@ -0,0 +1,32 @@
+package com.example.philoniare.musicalstructure.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.philoniare.musicalstructure.R;
+
+public class PlaylistViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Playlist Viewer");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
